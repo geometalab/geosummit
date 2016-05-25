be9bf933f8c59b279661a3ef14686811d1c886ba
Joël Schwab <jschwab@hsr.ch>
2016-05-25 11:32:22 +0200

    Added a Menu to change the Parts of the geosummit

---
+++ menu.js
@@ -0,0 +1,15 @@
+function buttonOneAction() {
+    document.getElementById("main").innerHTML = document.getElementById("part1").innerHTML;
+}
+
+function buttonTwoAction() {
+    document.getElementById("main").innerHTML = document.getElementById("part2").innerHTML;
+}
+
+function buttonThreeAction() {
+    document.getElementById("main").innerHTML = document.getElementById("part3").innerHTML;
+}
+
+function buttonFourAction() {
+    document.getElementById("main").innerHTML = document.getElementById("part4").innerHTML;
+}
