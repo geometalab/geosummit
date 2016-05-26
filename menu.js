a37622a86dc826f1d6841a35830758396cd6c44f
Joël Schwab <jschwab@hsr.ch>
2016-05-26 15:53:16 +0200

    Reworked Header

--- menu.js
+++ menu.js
@@ -1,23 +1,24 @@
 function buttonOneAction() {
     document.getElementById("main").innerHTML = document.getElementById("part1").innerHTML;
-    document.getElementById("dateText").textContent = "Mittwoch Morgen";
     document.getElementById("dateTextMob").textContent = "Mi Morgen";
 }
 
 function buttonTwoAction() {
     document.getElementById("main").innerHTML = document.getElementById("part2").innerHTML;
-    document.getElementById("dateText").textContent = "Mittwoch Nachmittag";
     document.getElementById("dateTextMob").textContent = "Mi Nachmittag";
 }
 
 function buttonThreeAction() {
     document.getElementById("main").innerHTML = document.getElementById("part3").innerHTML;
-    document.getElementById("dateText").textContent = "Donnerstag Morgen";
     document.getElementById("dateTextMob").textContent = "Do Morgen";
 }
 
 function buttonFourAction() {
     document.getElementById("main").innerHTML = document.getElementById("part4").innerHTML;
-    document.getElementById("dateText").textContent = "Donnerstag Nachmittag";
     document.getElementById("dateTextMob").textContent = "Do Nachmittag";
 }
+
+$(document).on("click", 'ul li', function(){
+    $('ul li').removeClass('active');
+    $(this).addClass('active');
+});
