9635f4ea169c1da29c2f58915f39e94312778c27
Mike Marti <mmarti@hsr.ch>
2016-05-27 12:54:57 +0200

    Increased maximum rooms per row to 7 Some small changes

--- menu.js
+++ menu.js
@@ -1,42 +1,45 @@
 function buttonOneAction() {
     document.getElementById("main").innerHTML = document.getElementById("part1").innerHTML;
     document.getElementById("dateTextMob").textContent = "Mi Morgen";
-    setGroupSizes()
+    setGroupSizes();
 }
 
 function buttonTwoAction() {
     document.getElementById("main").innerHTML = document.getElementById("part2").innerHTML;
     document.getElementById("dateTextMob").textContent = "Mi Nachmittag";
-    setGroupSizes()
+    setGroupSizes();
 }
 
 function buttonThreeAction() {
     document.getElementById("main").innerHTML = document.getElementById("part3").innerHTML;
     document.getElementById("dateTextMob").textContent = "Do Morgen";
-    setGroupSizes()
+    setGroupSizes();
 }
 
 function buttonFourAction() {
     document.getElementById("main").innerHTML = document.getElementById("part4").innerHTML;
     document.getElementById("dateTextMob").textContent = "Do Nachmittag";
-    setGroupSizes()
+    setGroupSizes();
 }
 
 $(window).resize(function () {
-    var tiles = $.makeArray($.merge($.merge(document.getElementById("main").getElementsByClassName("tile-group-before-break"), document.getElementById("main").getElementsByClassName("tile-group-after-break")), document.getElementById("main").getElementsByClassName("tile-group-empty")));
-
-    for(var i = 0; i < $(tiles).length; i++) {
-        var tile = $(tiles).get(i);
-        $(tile).css("height", "");
-    }
-
     setGroupSizes();
 });
 
 function setGroupSizes() {
-    tileHeightToHighest(document.getElementById("main").getElementsByClassName("tile-group-before-break"));
-    tileHeightToHighest(document.getElementById("main").getElementsByClassName("tile-group-after-break"));
-    tileHeightToHighest(document.getElementById("main").getElementsByClassName("tile-group-empty"));
+    var before = document.getElementById("main").getElementsByClassName("tile-group-before-break");
+    var after = document.getElementById("main").getElementsByClassName("tile-group-after-break");
+    var empty = document.getElementById("main").getElementsByClassName("tile-group-empty");
+    var tileGroups = $.makeArray($.merge($.merge(before, after), empty));
+
+    for(var i = 0; i < $(tileGroups).length; i++) {
+        var tile = $(tileGroups).get(i);
+        $(tile).css("height", "");
+    }
+
+    tileHeightToHighest(before);
+    tileHeightToHighest(after);
+    tileHeightToHighest(empty);
 }
 
 function getBiggestTileHeight(tiles) {
