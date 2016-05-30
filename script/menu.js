f0d064979a160aba8fff5d8e08624538836ccb7b
Mike Marti <mmarti@hsr.ch>
2016-05-30 12:09:38 +0200

    Moved files Fixed width on wednesday

--- script/menu.js
+++ script/menu.js
@@ -58,13 +58,13 @@ function upDateSite() {
     //getDay() Mittwoch 3, Do 4
     var now = new Date();
     if(now.getDay() == 3 && now.getHours() < 10 && now.getMinutes() < 55){
-        window.location.href= "index.html";
+        window.location.href= "../index.html";
     } else if(now.getDay() == 3 && now.getHours() > 14 && now.getMinutes() > 25) {
-        window.location.href= "wednesday-afternoon.html";
+        window.location.href= "../wednesday-afternoon.html";
     } else if(now.getDay() == 4 && now.getHours() < 10 && now.getMinutes() < 55) {
-        window.location.href= "thursday-morning.html";
+        window.location.href= "../thursday-morning.html";
     } else if(now.getDay() == 4 && now.getHours() > 14 && now.getMinutes() > 25) {
-        window.location.href= "thursday-afternoon.html";
+        window.location.href= "../thursday-afternoon.html";
     } else {
         window.location.href = "http://megageogame2.herokuapp.com/"
     }
